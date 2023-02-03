08c6a20f7dc6be89f774090bf671bfc660e4544d
Angela <angzla329@gmail.com>
2023-02-03 11:31:38 -0800

    fixed linter issues

--- Fixme.py
+++ Fixme.py
@@ -3,5 +3,5 @@ def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
-    xs = list(filter(lambda x: x % 2 == 0, list(range(0, n+1))))
+    xs = list(filter(lambda x: x % 2 == 0, list(range(0, n + 1))))
     return xs
